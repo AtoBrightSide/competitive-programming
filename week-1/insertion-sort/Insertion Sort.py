de3087e2542231de02556425b5a1a552b23808b9
AtoBrightSide <meseleabel99@gmail.com>
2021-12-12 23:02:43 +0300

    improved time for insertion p1

--- week-1/insertion-sort/Insertion Sort.py
+++ week-1/insertion-sort/Insertion Sort.py
@@ -4,18 +4,14 @@ def insertionSort1(n, arr):
     for i in range(len(arr) - 2, -1, -1):
         if stored > arr[i]:
             arr[i + 1] = stored
-            for num in arr:
-                print(num, end=" ")
+            print(*arr)
             break
         else:
             arr[i + 1] = arr[i]
-            for num in arr:
-                print(num, end=" ")
-        print("")
+            print(*arr)
         if i == 0:
             arr[i] = stored
-            for num in arr:
-                print(num, end=" ")
+            print(*arr)
 
 
 insertionSort1(10, [2, 3, 4, 5, 6, 7, 8, 9, 10, 1])
