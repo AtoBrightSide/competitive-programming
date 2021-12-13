a5cd55f475967dbcbdcbe5cbb6e4647e2e92f883
AtoBrightSide <meseleabel99@gmail.com>
2021-12-13 23:09:41 +0300

    sorted the sentence

---
+++ week-1/leetcode/1859. Sorting the Sentence.py
@@ -0,0 +1,14 @@
+def sortSentence(s):
+    s = s.split(" ")
+    for i in range(len(s)):
+        for j in range(len(s) - 1):
+            if int(s[j][len(s[j]) - 1]) > int(s[j + 1][len(s[j + 1]) - 1]):
+                s[j], s[j + 1] = s[j + 1], s[j]
+    sorted = ""
+    for word in s:
+        word = word.replace(word[len(word) - 1], " ")
+        sorted += word
+    print(sorted[:len(sorted) - 1])
+
+
+sortSentence("is2 sentence4 This1 a3")
