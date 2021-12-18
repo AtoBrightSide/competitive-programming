99054c6314c6540df9e0ba48c7aea79d4f93f377
AtoBrightSide <meseleabel99@gmail.com>
2021-12-18 15:09:18 +0300

    finally did the largest number

---
+++ week-1/leetcode/179. Largest Number.py
@@ -0,0 +1,13 @@
+class Large(str):
+    def __lt__(a,b):
+        return a+b<b+a
+    
+def largestNumber(nums):
+    counter=0
+    nums=[str(num) for num in nums]
+    nums.sort(key= Large, reverse=True)
+    res = "".join(nums)
+    for num in nums:
+        counter += 1 if num=='0' else 0
+    return res if counter != len(nums) else '0'
+print(largestNumber([0,0,0,0]))
