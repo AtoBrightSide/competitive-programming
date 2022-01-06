8d1b22a954964b45a4971983fc49daf673642931
AtoBrightSide <meseleabel99@gmail.com>
2022-01-06 17:23:00 +0300

    did the power of four question recursively

---
+++ week-3/leetcode/342. Power of Four.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return self.recur(n)
+
+    def recur(self, n):
+        if n == 1:
+            return True
+        elif n < 1:
+            return False
+        return self.recur(n / 4)
