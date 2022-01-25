59e5175debcb45f881679c974797f442f10401d9
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-01-25 07:54:21 +0300

    tried to optimize the delete node question

--- week-6/237. Delete Node in a Linked List.py
+++ week-6/237. Delete Node in a Linked List.py
@@ -10,4 +10,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        node = node.next
+        node.val = node.next.val
+        node.next = node.next.next
+        
