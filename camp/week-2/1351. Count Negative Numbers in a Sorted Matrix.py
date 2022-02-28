369e1d4b2fb900205508b6c9f7d1ee57fa7ac417
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-02-28 15:23:48 +0300

    used binary search and solved in O(nlogn)

--- camp/week-2/1351. Count Negative Numbers in a Sorted Matrix.py
+++ camp/week-2/1351. Count Negative Numbers in a Sorted Matrix.py
@@ -1,8 +1,20 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        count = 0
-        for col in grid:
-            for elt in col:
-                count += 1 if elt<0 else 0
-                
-        return count
+        ans=0
+        for row in grid:
+            ans += self.myBS(row)
+        return ans
+    
+    def myBS(self, lst):
+        l, r, best = 0, len(lst)-1, len(lst)
+        while l<=r:
+#           md = (l+r)//2
+#           the below one covers everything
+            md = l + (r-l)//2
+            if lst[md] < 0:
+                r = md-1
+                best = md
+            else:
+                l = md + 1
+
+        return len(lst)-best
