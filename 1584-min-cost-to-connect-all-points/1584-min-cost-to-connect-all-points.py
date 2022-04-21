c9a0f0fa95f7270347d072b1f6061126b5c4af84
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-04-21 17:00:44 +0300

    Time: 2913 ms (47.60%), Space: 81.7 MB (69.85%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        arr, res = [], 0
+        
+        parent = [i for i in range(len(points))]
+        rank = [1]*len(points)
+        
+        def find(node):
+            if parent[node] == node:
+                return node
+            parent[node] = find(parent[node])
+            return find(parent[node])
+        
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i, len(points)):
+                if i != j:
+                    x2, y2 = points[j]
+                    arr.append((abs(x2-x1) + abs(y2-y1), i, j))
+        
+        arr.sort()
+        for mtd, i, j in arr:
+            parentA = find(i)
+            parentB = find(j)
+            if parentA != parentB:
+                res += mtd
+                if rank[parentB] >= rank[parentA]:
+                    parent[parentB] = parentA
+                    rank[parentA] += rank[parentB]
+                    rank[parentB] = 1
+                else:
+                    parent[parentA] = parentB
+                    rank[parentB] += rank[parentA]
+                    rank[parentA] = 1
+        
+        return res
