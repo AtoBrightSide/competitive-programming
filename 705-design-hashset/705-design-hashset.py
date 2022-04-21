0362fd8838796e88419a062806555f14ac7afe57
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-04-21 18:21:33 +0300

    Time: 4708 ms (5.01%), Space: 18.5 MB (82.52%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,14 +1,20 @@
 class MyHashSet:
 
     def __init__(self):
-        self.myset = set()
+        self.myset = []
 
     def add(self, key: int) -> None:
-        self.myset.add(key)
+        if key in self.myset:
+            return
+        self.myset.append(key)
+            
 
     def remove(self, key: int) -> None:
-        if key in self.myset:
-            self.myset.remove(key)
+        for i in range(len(self.myset)):
+            if self.myset[i] == key:
+                self.myset[i] = self.myset[-1]
+                self.myset.pop()
+                return
 
     def contains(self, key: int) -> bool:
         return key in self.myset
