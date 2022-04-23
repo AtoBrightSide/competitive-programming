92531df178f77fbaba9debf80d1e1170c0e297de
AtoBrightSide <meseleabel99@gmail.com>
2022-04-23 11:18:48 +0300

    number of provinces

---
+++ week-9/547. Number of Provinces.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        parent = [i for i in range(len(isConnected))]
+        ranks = [1] * len(isConnected)
+        provinces = len(isConnected)
+
+        def find(node):
+            if parent[node] == node:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+
+        def unite(i, j):
+            parentI = find(i)
+            parentJ = find(j)
+            nonlocal provinces
+            if parentI != parentJ:
+                provinces -= 1
+                if ranks[parentI] >= ranks[parentJ]:
+                    parent[parentJ] = parentI
+                    ranks[parentI] += ranks[parentJ]
+                    ranks[parentJ] = 0
+                else:
+                    parent[parentI] = parentJ
+                    ranks[parentJ] += ranks[parentI]
+                    ranks[parentI] = 0
+
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected)):
+                if isConnected[i][j] == 1:
+                    unite(i, j)
+
+        return provinces
