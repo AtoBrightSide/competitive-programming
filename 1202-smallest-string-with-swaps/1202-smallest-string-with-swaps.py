57cef6f03dc64c36e3c17bf69f51505b41d4196d
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-04-28 00:34:11 +0300

    Time: 950 ms (48.16%), Space: 50.6 MB (31.64%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,38 +1,46 @@
-class Solution(object):
-    def smallestStringWithSwaps(self, s, pairs):
-        """
-        :type s: str
-        :type pairs: List[List[int]]
-        :rtype: str
-        """
-        from collections import defaultdict
-        d = defaultdict(list)
-        s = list(s)
-        visited = [False for _ in range(len(s))]
-
-        for source, destination in pairs:
-            d[source].append(destination)
-            d[destination].append(source)
-
-        def dfs(s, i, chars, indices):
-            if visited[i]:
-                return
-            chars.append(s[i])
-            indices.append(i)
-            visited[i] = True
-
-            for neigh in d[i]:
-                dfs(s, neigh, chars, indices)
-
-        for i in range(len(s)):
-            if not visited[i]:
-                chars = []
-                indices = []
-
-                dfs(s, i, chars, indices)
-                chars = sorted(chars)
-                indices = sorted(indices)
-                for c, i in zip(chars, indices):
-                    s[i] = c
-
-        return "".join(s)
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        parent = [i for i in range(len(s))]
+        rank = [1 for i in range(len(s))]
+        
+        def find(node):
+            # nonlocal parent
+            if parent[node] == node:
+                return node
+            parent[node] = find(parent[node])
+            return parent[node]
+            
+        def unite(x, y):
+            # nonlocal rank
+            
+            px = find(x)
+            py = find(y)
+            
+            if px != py:
+                if rank[px] >= rank[py]:
+                    parent[py] = px
+                    rank[px] += rank[py]
+                    rank[py] = 0
+                else:
+                    parent[px] = py
+                    rank[py] += rank[px]
+                    rank[px] = 0
+                    
+        myDict = defaultdict(list)
+        for x,y in pairs:
+            unite(x,y)
+        
+        print(parent)
+        for i in range(len(parent)):
+            myDict[find(parent[i])].append(i)
+        
+        res = [0]*len(s)
+        
+        for key in myDict.keys():
+            chars = [s[i] for i in myDict[key]]
+            chars.sort()
+            for i in range(len(chars)):
+                res[myDict[key][i]] = chars[i]
+                
+        return "".join(res)
+    
