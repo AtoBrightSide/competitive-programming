d0fb5b5861376ca083e625ea6ae26ee7d2a29227
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-04-28 00:36:44 +0300

    Time: 841 ms (62.58%), Space: 50.5 MB (69.63%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,36 +1,32 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         parent = [i for i in range(len(s))]
-        rank = [1 for i in range(len(s))]
+        # rank = [1 for i in range(len(s))]
         
         def find(node):
-            # nonlocal parent
             if parent[node] == node:
                 return node
             parent[node] = find(parent[node])
             return parent[node]
             
         def unite(x, y):
-            # nonlocal rank
-            
             px = find(x)
             py = find(y)
             
             if px != py:
-                if rank[px] >= rank[py]:
-                    parent[py] = px
-                    rank[px] += rank[py]
-                    rank[py] = 0
-                else:
-                    parent[px] = py
-                    rank[py] += rank[px]
-                    rank[px] = 0
+                # if rank[px] >= rank[py]:
+                parent[py] = px
+                #     rank[px] += rank[py]
+                #     rank[py] = 0
+                # else:
+                #     parent[px] = py
+                #     rank[py] += rank[px]
+                #     rank[px] = 0
                     
         myDict = defaultdict(list)
         for x,y in pairs:
             unite(x,y)
         
-        print(parent)
         for i in range(len(parent)):
             myDict[find(parent[i])].append(i)
         
