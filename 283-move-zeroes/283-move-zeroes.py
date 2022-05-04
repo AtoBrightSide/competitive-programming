2cd7e78703e0ba5cb40b96981509846e8d786f1d
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-05-04 11:16:49 +0300

    Time: 365 ms (20.43%), Space: 15.7 MB (18.53%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,11 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        slow = fast = count = 0
+        slow = fast = 0
         while fast < len(nums):
-            if nums [fast] == 0:
-                count += 1    
-                fast += 1
+            if nums [fast] == 0:    fast += 1
             else:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
                 fast += 1
