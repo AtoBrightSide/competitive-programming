ee77f0ff5f153854f10df553c1204811485c791b
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-05-04 14:04:24 +0300

    Time: 265 ms (29.73%), Space: 38.2 MB (5.04%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self):
+        self.children = [None] *26
+        self.end = False
+        
+class TrieClass:
+    def __init__(self):
+        self.root = Node()
+    
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            letter = ord(ch) - ord('a')
+            if not curr.children[letter]:
+                curr.children[letter] = Node()
+            curr = curr.children[letter]
+        curr.end = True
+    
+    def startsWith(self, word, flag):
+        curr = self.root
+        root = ""
+        for ch in word:
+            letter = ord(ch) - ord('a')
+            if not curr.children[letter]:
+                return word
+            else:
+                root += chr(letter + ord('a'))
+                curr = curr.children[letter]
+                if curr.end:
+                    return root
+            
+        return word
+        
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = TrieClass()
+        for word in dictionary:
+            trie.insert(word)
+        ans = ""
+        sentence = sentence.split()
+        for word in sentence:
+            ans += " " + trie.startsWith(word, False)
+                
+        return ans[1:]
