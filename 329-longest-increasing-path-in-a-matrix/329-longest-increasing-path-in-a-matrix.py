dfb35a4348bc3a521b0f9346b44c187e029251e6
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-05-20 14:26:00 +0300

    Time: 507 ms (75.72%), Space: 18.5 MB (30.18%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -6,24 +6,20 @@ class Solution:
             return 0 <= i < len(matrix) and 0 <= j < len(matrix[i])
         
         from_here = {}
+        @lru_cache(None)
         def dfs(i, j, count):
-            if (i, j) in from_here:
-                return from_here[(i, j)]
             best = 0
             for x, y in dirs:
                 x_i, y_j = x+i, y+j
                 if inBounds(x_i, y_j) and matrix[x_i][y_j] > matrix[i][j]:
                     best = max(best, count + dfs(x_i, y_j, count))
-            from_here[(i, j)] = max(count, best)
             
-            return from_here[(i, j)]
+            return max(count, best)
+            
             
         maxPath = 0
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if (i, j) in from_here:
-                    maxPath = max(maxPath, from_here[(i, j)])
-                else:
-                    maxPath = max(maxPath, dfs(i, j, 1))
+                maxPath = max(maxPath, dfs(i, j, 1))
                 
         return maxPath
