cc5d434f7a4614aabd29bd3e25109ad256d67240
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-05-26 12:22:56 +0300

    Time: 139 ms (76.65%), Space: 14.2 MB (84.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,16 +1,24 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:        
-        memo, longest = {len(nums) - 1: 1}, -1
-
-        @lru_cache(None)
-        def dfs(index):                
-            longest = 1
-            for i in range(index + 1, len(nums)):
-                if nums[i] > nums[index]:   
-                    longest = max(longest, 1 + dfs(i))
-
-            return longest
+        sequence = [float('inf')] * len(nums)
+        sequence[0] = nums[0]
         
-        for i in range(len(nums)):
-            longest = max(longest, dfs(i))
-        return longest
+        def binarySearch(num):
+            l, r = 0, len(nums) - 1
+            
+            while l <= r:
+                md = l + (r - l)//2
+                if sequence[md] >= num:
+                    r = md - 1
+                else:
+                    l = md + 1
+            return l
+        
+        
+        for i in range(1, len(nums)):
+            pos = binarySearch(nums[i])
+            sequence[pos] = nums[i]
+            
+        
+        return len(sequence) - sequence.count(float('inf'))
+        
