355ad69a31e43e4c45877dcd95f4cd674ee0fe4f
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-06-01 11:33:54 +0300

    Time: 2305 ms (32.02%), Space: 43.9 MB (6.07%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,22 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        def dp(is_needed):
-            if is_needed == 0:    return 0
-            
-            if is_needed in memo: return memo[is_needed]
-            
-            best = amount + 1
-            for coin in coins:
-                if is_needed - coin >= 0: 
-                    best = min(best, 1 + dp(is_needed - coin))
-            
-            memo[is_needed] = best
-            return memo[is_needed]
-            
-        for num in range(amount):
-            dp(num)
-            
+        if amount == 0: return 0
         
-        ans = dp(amount)
-        return ans if ans != amount + 1 else -1
+        memo = {}
+        def dfs(remainder):
+            if remainder == 0:  return 0
+            
+            if remainder in memo:   return memo[remainder]
+            
+            best = float('inf')
+            for coin in coins:
+                curr = remainder - coin
+                if curr >= 0:   best = min(1 + dfs(curr), best)
+                    
+            memo[remainder] = best
+            return memo[remainder]
+        
+        best = float('inf')
+        for coin in coins:
+            best = min(best, 1 + dfs(amount - coin))
+            
+        return -1 if best == float('inf') else best
