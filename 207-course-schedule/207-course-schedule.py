4525b9f2462240a5105f47711e7f28a807940243
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-06-24 02:37:20 +0300

    Time: 158 ms (42.02%), Space: 15.6 MB (68.26%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        course_2_take = [0] * numCourses
-        graph = {}
-        for course, preq in prerequisites:
-            course_2_take[course] += 1
-            if preq in graph:
-                graph[preq].append(course)
-            else:
-                graph[preq] = [course]
-        myQ = deque()
-        for i in range(len(course_2_take)):
-            if course_2_take[i] == 0:
-                myQ.append(i)
-        count = 0
-        while myQ:
-            curr = myQ.popleft()
-            count += 1
-            if curr in graph:
-                for node in graph[curr]:
-                    print(f"node -> {node}")
-                    course_2_take[node] -= 1
-                    if course_2_take[node] == 0:
-                        myQ.append(node)
+        indegrees = defaultdict(int)
+        outgoing = defaultdict(list)
+        
+        for c, p in prerequisites:
+            indegrees[p] += 1
+            outgoing[c].append(p)
             
+        queue = deque([])
+        for i in range(numCourses):
+            if indegrees[i] == 0:    queue.append(i)
+
+        count = 0
+        while queue:
+            curr = queue.popleft()
+            count += 1
+            for c in outgoing[curr]:
+                indegrees[c] -= 1
+                if indegrees[c] == 0:   queue.append(c)
+        
         return count == numCourses
