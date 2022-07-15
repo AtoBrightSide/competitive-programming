287f353dcb3546ba6129bb963113897f417f52cc
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-07-15 23:21:34 +0300

    Time: 461 ms (5.02%), Space: 17.9 MB (9.21%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,25 +1,41 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         isValid = lambda i,j: 0 <= i < len(grid) and 0 <= j < len(grid[0])
+        DIRS = [[0,1],[0,-1],[1,0],[-1,0]]
+        rank, parent = {}, {}
         
-        DIRS = [[0,1],[1,0],[0,-1],[-1,0]] 
-        visited = set()
-        
-        def dp(i, j):
-            count = 0
-            visited.add((i, j))
-            for d in DIRS:
-                x, y = d[0] + i, d[1] + j
-                if isValid(x, y) and (x, y) not in visited and grid[x][y] == 1:
-                    count += 1 + dp(x, y)
-                    
-            return count
-        
-        max_area = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    max_area = max(max_area, 1 + dp(i, j))
+                rank[(i,j)] = 0 if grid[i][j] == 0 else 1
+                parent[(i,j)] = (i,j)
+        
+        def find(i,j):
+            if (i, j) == parent[(i,j)]:
+                return i,j
             
+            i,j = find(parent[(i, j)][0], parent[(i, j)][1])
+            return i, j
+        
+        def unite(i1, j1, i2, j2):
+            p1 = find(i1, j1)
+            p2 = find(i2, j2)
             
-        return max_area
+            if p1 != p2:
+                if rank[p1] >= rank[p2]:
+                    parent[p2] = p1
+                    rank[p1] += rank[p2]
+                    rank[p2] = 0
+                else:
+                    parent[p1] = p2
+                    rank[p2] += rank[p1]
+                    rank[p1] = 0
+                    
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                for d in DIRS:
+                    x, y = d[0] + i, d[1] + j
+                    if grid[i][j] == 1 and isValid(x,y) and grid[x][y] == 1:    unite(i, j, x, y)
+        
+        ans = max(rank.values())
+        
+        return ans
