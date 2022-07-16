0a690f69c8eb96b5b68def47eaaccda1c8749367
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-07-17 00:14:07 +0300

    Time: 201 ms (53.20%), Space: 20.7 MB (8.44%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -3,20 +3,17 @@ class Solution:
         isOut = lambda i,j: i < 0 or i >= m or j < 0 or j >= n 
         DIRS = [[0,1],[1,0],[0,-1],[-1,0]]
         
-        memo = {}
+        @cache
         def dfs(i, j, moves):
             if isOut(i,j):      return 1
             
             if moves == 0:  return 0
-            
-            if (i, j, moves) in memo:   return memo[(i, j, moves)]
             
             count = 0
             for d in DIRS:
                 x, y = d[0] + i, d[1] + j
                 count += dfs(x, y, moves - 1)
                 
-            memo[(i, j, moves)] = count
-            return memo[(i, j, moves)]
+            return count
         
         return dfs(startRow, startColumn, maxMove) % ((10**9) + 7)
