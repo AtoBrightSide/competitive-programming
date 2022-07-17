407c989c23bc648a6d4242b3231cd2883ed5387e
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-07-17 15:49:09 +0300

    Time: 1039 ms (39.68%), Space: 36.5 MB (54.39%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -5,15 +5,45 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        nodes = []
-        while head:
-            nodes.append(head.val)
-            head = head.next
+        def merge(list1, list2):
+            ans = dummy = ListNode(0)
+            while list1 or list2:
+                if list1 and list2:
+                    if list1.val <= list2.val:
+                        dummy.next = list1
+                        list1 = list1.next
+                    else:
+                        dummy.next = list2
+                        list2 = list2.next
+                    dummy = dummy.next
+                elif list1:
+                    dummy.next = list1
+                    list1 = list1.next
+                    dummy = dummy.next
+                elif list2:
+                    dummy.next = list2
+                    list2 = list2.next
+                    dummy = dummy.next
+            return ans.next
         
-        nodes.sort()
-        ans = dummy = ListNode(0)
-        for n in nodes:
-            dummy.next = ListNode(n)
-            dummy = dummy.next
+        def getMid(list1):
+            fast = list1
+            while fast and fast.next:
+                fast = fast.next.next
+                list1 = list1.next if fast and fast.next else list1
+            
+            return list1
         
-        return ans.next
+        def recur(head):
+            if not head or not head.next:    return head
+            
+            mid = getMid(head)
+            temp = mid.next
+            mid.next = None
+            
+            left = recur(head)
+            right = recur(temp)
+            
+            return merge(left, right)
+        
+        return recur(head)
