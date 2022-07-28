4390659b95bee9a097cf95fef1ae1fe31815dd4d
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-07-28 23:26:35 +0300

    Time: 231 ms (29.90%), Space: 16 MB (41.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,18 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        tracker = defaultdict(lambda: [float(inf), float(inf)])
-        lmax = rmax = -float(inf)
-        for i, num in enumerate(height):
-            tracker[i][0] = lmax
-            lmax = max(lmax, num)
-        for i in range(len(height)-1, -1, -1):
-            tracker[i][1] = rmax
-            rmax = max(rmax, height[i])
+        # highest from left and right
+        L = len(height)-1
+        left_high, right_high = [0] * (L+1), [0] * (L+1)
+        for i in range(1, len(height)):
+            left_high[i] = max(left_high[i-1], height[i-1])
+            right_high[L-i] = max(right_high[L-i+1], height[L-i+1])
         
         trapped = 0
-        for i, val in tracker.items():
-            curr = min(val)
-            if i != 0 and i != len(height) - 1 and curr > height[i]:
-                trapped += (curr - height[i])
-                
+        for i, h in enumerate(height):
+            curr = min(left_high[i], right_high[i])
+            trapped += curr - (h if h < curr else curr)
+            
         return trapped
