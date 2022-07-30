73c8727b44d9fea3860080774f5a6b0c3411ac27
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-07-30 21:47:53 +0300

    Time: 1174 ms (59.00%), Space: 18.7 MB (38.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,27 +1,24 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        subs = set()
         sub_counter = defaultdict(int)
         for w in words2:
             curr = defaultdict(int)
             for ch in w:    
-                subs.add(ch)
                 curr[ch] += 1
             for ch in w:
                 sub_counter[ch] = max(sub_counter[ch], curr[ch])
         
-        words2 = list(subs)
-        
+        words1 = set(words1)
         L = len(words2)
+        
         universal = []
         for word in words1:
-            if L <= len(word):
-                word_counter = Counter(word)
-                flag = True
-                for ch in words2:
-                    if ch not in word or word_counter[ch] < sub_counter[ch]:  
-                        flag = False
-                if flag:    
-                    universal.append(word)
+            word_counter = Counter(word)
+            flag = True
+            for ch, count in sub_counter.items():
+                if ch not in word or word_counter[ch] < count:  
+                    flag = False
+
+            if flag:        universal.append(word)
                 
         return universal
