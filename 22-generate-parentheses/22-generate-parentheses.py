80648cc86e6bdd135be222c072306729ea3be869
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-10-06 05:32:23 +0300

    Time: 93 ms (7.79%), Space: 14.2 MB (77.16%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,29 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def isValid(comb):
-            stack = []
-            for ch in comb:
-                if ch == "(":
-                    stack.append("(")
-                else:
-                    if not stack:   return False
-                    stack.pop()
-            return stack == []
-        
         res = []
-        def backtrack(comb):
+        def backtrack(comb, op, cl):
             if len(comb) == 2 * n:
-                if isValid(comb):   res.append("".join(comb))
+                res.append("".join(comb))
                 return
             
-            comb.append("(")
-            backtrack(comb)
-            comb.pop()
-            
-            comb.append(")")
-            backtrack(comb)
-            comb.pop()
+            if op:
+                comb.append("(")
+                backtrack(comb, op - 1, cl)
+                comb.pop()
+            if cl > op:
+                comb.append(")")
+                backtrack(comb, op, cl - 1)
+                comb.pop()
         
-        backtrack([])
+        backtrack([], n, n)
         return res
         
