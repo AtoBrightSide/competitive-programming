9c0e795d7df52f17ee9d65e11d4e39067173a930
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-10-17 12:05:16 +0300

    Time: 2374 ms (33.46%), Space: 21.6 MB (20.11%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,30 +1,30 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        can_use = {}
-        for i in range(len(ingredients)):
-            for j in range(len(ingredients[i])):
-                if ingredients[i][j] in can_use:
-                    can_use[ingredients[i][j]].append(recipes[i])
-                else:
-                    can_use[ingredients[i][j]] = [recipes[i]]
+        # processing denoted by 1
+        # done denoted by 2
+        # initial state denoted by 0
         
-        to_make = {}
-        for i, rec in enumerate(recipes):
-            to_make[rec] = len(ingredients[i])
+        graph = defaultdict(list)
+        for i, recipe in enumerate(recipes):
+            for ingd in ingredients[i]:
+                graph[recipe].append(ingd)
+                
+        curr_state = defaultdict(int)
+        supplies = set(supplies)        
+        def dfs(meal):
+            if curr_state[meal] == 2 or meal in supplies:   return True
+            elif curr_state[meal] == 1:   return False
             
-        recipes = set(recipes)
-        queue = deque()
+            can_make = True
+            curr_state[meal] = 1
+            for ing in graph[meal]:
+                can_make = can_make and dfs(ing)
+            
+            curr_state[meal] = 2 if can_make and len(graph[meal]) > 0 else 0
+            return can_make if len(graph[meal]) > 0 else False
+        
         ans = []
-        for sup in supplies:
-            queue.append(sup)
-        while queue:
-            curr = queue.popleft()
-            if curr in recipes:
-                ans.append(curr)
-            if curr in can_use:
-                for meal in can_use[curr]:
-                    to_make[meal] -= 1
-                    if to_make[meal] == 0:
-                        queue.append(meal)
-                    
+        for recipe in recipes:  
+            if dfs(recipe): ans.append(recipe)
+        
         return ans
