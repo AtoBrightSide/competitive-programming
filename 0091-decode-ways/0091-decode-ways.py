03fea6a9230e7b9f94186d2e038c6294af337594
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-10-26 09:19:17 +0300

    Time: 63 ms (43.53%), Space: 14 MB (46.37%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,16 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        mapper = {str(i):chr(i+96) for i in range(1, 27)}
         
-        @cache
-        def dp(i):
-            if i >= len(s):     return 1
-            if s[i] == '0':     return 0
-            
-            curr = dp(i+1)
-            if i < len(s) - 1 and (str(s[i]) + str(s[i + 1])) in mapper:
-                curr += dp(i+2)
-            
-            return curr
+        dp = [1 if ch != '0' else 0 for ch in s]
+        dp.append(1)
+        for i in range(len(s) - 2, -1, -1):
+            if dp[i] != 0:
+                if 1 <= int(s[i:i+2]) <= 26:
+                    dp[i] = dp[i+1] + (dp[i+2] if (i + 2) < len(s) else 1)
+                else:
+                    dp[i] = dp[i+1]
         
-        return dp(0)
+        return dp[0]
+        
+    
+    
