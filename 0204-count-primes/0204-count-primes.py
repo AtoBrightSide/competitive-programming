4be648f84eeec06e1552c456e8c174e84a5666f2
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-11-04 13:58:07 +0300

    Time: 2822 ms (79.55%), Space: 52.6 MB (96.83%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        nums = [0] * n
+        for i in range(2, int(n**0.5) + 1):
+            if nums[i]: continue
+            
+            for j in range(i * i, n, i):
+                nums[j] = 1
+        
+        return max(0, nums.count(0) - 2)
