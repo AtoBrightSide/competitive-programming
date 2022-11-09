36749c08d127e5b79ed8eac0a6c416adc8287ff8
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-11-09 21:40:23 +0300

    Time: 243 ms (81.40%), Space: 15.3 MB (95.85%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,3 +1,14 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        return [k for k, val in Counter(nums).items() if val == 2] + [int((len(nums) * (len(nums) + 1))/2) - sum(set(nums))]
+        ans = []
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] < 0: 
+                ans.append(abs(nums[i]))
+            else:
+                nums[abs(nums[i]) - 1] *= -1
+        
+        for i, num in enumerate(nums):
+            if num > 0: 
+                ans.append(i+1)
+        
+        return ans
