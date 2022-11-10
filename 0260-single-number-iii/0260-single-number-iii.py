1a28a1b447e3e8fd901b08b66db4d587b2997932
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2022-11-10 22:14:53 +0300

    Time: 138 ms (39.84%), Space: 15.7 MB (83.02%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,3 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        return [key for key, val in Counter(nums).items() if val == 1]
+        num = 0
+        for x in nums:
+            num ^= x
+        
+        pos = int(math.log2(num & ~(num - 1)))
+        ans = [0, 0]
+        
+        for x in nums:
+            if not x ^ ((1 << pos) | x):
+                ans[0] ^= x
+        
+        ans[1] = ans[0] ^ num
+        return ans
