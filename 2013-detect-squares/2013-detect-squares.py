1a38ae97f7b3faa8066c9c7789f6a7eba4dc9ea6
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2023-02-07 00:22:29 +0300

    Time: 270 ms (90.49%), Space: 16 MB (41.72%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -9,24 +9,19 @@ class DetectSquares:
         self.x[x].add(y)
         self.square[(x, y)] += 1
 
-    def check(self, x1, y1, x2, y2, L):
-        side1 = (x1 + L, y1)
-        side3 = (x2 + L, y2)
-        
-        _side1 = (x1 - L, y1)
-        _side3 = (x2 - L, y2)
-        
-        ans = 0
-        ans += self.square[side1] * self.square[(x2, y2)] * self.square[side3]
-        ans += self.square[_side1] * self.square[(x2, y2)] * self.square[_side3]
-        
-        return ans
-    
     def count(self, point: List[int]) -> int:
         num_of_squares = 0
-        x, y = point
-        for y_pt in self.x[x]:
-            if y_pt != y:
-                num_of_squares += self.check(x, y, x, y_pt, abs(y - y_pt))
+        x1, y1 = point
+        for y2 in self.x[x1]:
+            L = abs(y2 - y1)
+            if y1 != y2:
+                side1 = (x1 + L, y1)
+                side3 = (x1 + L, y2)
+
+                _side1 = (x1 - L, y1)
+                _side3 = (x1 - L, y2)
+                
+                num_of_squares += self.square[side1] * self.square[(x1, y2)] * self.square[side3]
+                num_of_squares += self.square[_side1] * self.square[(x1, y2)] * self.square[_side3]
         
         return num_of_squares
