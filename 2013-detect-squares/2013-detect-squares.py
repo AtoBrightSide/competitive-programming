96f45a5172dec35adfed15d2370daf77fcd1628c
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2023-02-07 00:13:47 +0300

    Time: 265 ms (91.62%), Space: 15.9 MB (60.23%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,18 +1,32 @@
 class DetectSquares:
+
     def __init__(self):
-        self.square = Counter()
-        self.x = defaultdict(Counter)   # x -> all y coordinates with freqs
-
-    def add(self, point):
+        self.x = defaultdict(set)
+        self.square = defaultdict(int)
+        
+    def add(self, point: List[int]) -> None:
         x, y = point
-        self.square[x, y] += 1
-        self.x[x][y] += 1
+        self.x[x].add(y)
+        self.square[(x, y)] += 1
 
-    def count(self, point):
-        x, y = point
+    def check(self, x1, y1, x2, y2, L):
+        side1 = (x1 + L, y1)
+        side3 = (x2 + L, y2)
+        
+        _side1 = (x1 - L, y1)
+        _side3 = (x2 - L, y2)
+        
         ans = 0
-        for y2 in self.x[x]:
-            if y == y2: continue
-            ans += self.square[x, y2] * self.square[x + y2 - y, y] * self.square[x + y2 - y, y2]
-            ans += self.square[x, y2] * self.square[x + y - y2, y] * self.square[x + y - y2, y2]
+        ans += self.square[side1] * self.square[(x2, y2)] * self.square[side3]
+        ans += self.square[_side1] * self.square[(x2, y2)] * self.square[_side3]
+        
         return ans
+    
+    def count(self, point: List[int]) -> int:
+        num_of_squares = 0
+        x, y = point
+        for y_pt in self.x[x]:
+            if y_pt != y:
+                num_of_squares += self.check(x, y, x, y_pt, abs(y - y_pt))
+        
+        return num_of_squares
