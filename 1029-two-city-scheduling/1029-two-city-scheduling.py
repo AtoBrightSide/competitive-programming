ff875e75359410b1c42de5ffd45ada5b4b77e897
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2023-02-16 14:31:22 +0300

    Time: 49 ms (35.60%), Space: 13.9 MB (33.58%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,19 +1,13 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        L = len(costs)
-        memo = {}
-        def recur(a, b, i):
-            if i >= len(costs):
-                return 0
-            if (a, b) not in memo:
-                goToA = goToB = float("inf")
-                if a < L / 2:
-                    goToA = costs[i][0] + recur(a + 1, b, i + 1)
-                if b < L / 2:
-                    goToB = costs[i][1] + recur(a, b + 1, i + 1)
-                
-                memo[(a, b)] = min(goToA, goToB)
-            
-            return memo[(a, b)]
+        min_cost = 0
+        cost_diff = sorted([[costs[i][1] - costs[i][0], i] for i in range(len(costs))])
         
-        return recur(0, 0, 0)
+        for i, curr in enumerate(cost_diff):
+            diff, idx = curr
+            if i < len(cost_diff) / 2:
+                min_cost += costs[idx][1]
+            else:
+                min_cost += costs[idx][0]
+        
+        return min_cost
