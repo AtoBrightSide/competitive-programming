61573a489b7cd09edca600feeda1842441fc9203
Abel Mesele <61546304+AtoBrightSide@users.noreply.github.com>
2023-03-31 09:10:40 +0300

    Time: 8042 ms (42.74%), Space: 13.9 MB (43.98%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        DIRS = [[0, 1], [1, 0], [0,-1], [-1, 0]]
-        inBounds = lambda i, j: 0 <= i < len(board) and 0 <= j < len(board[0])
-        L = len(word)
+        DIRS = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        inBounds = lambda row, col: 0 <= row < len(board) and 0 <= col < len(board[0])
         
-        def backtrack(i, j, count):
-            if count == L:
+        def traverseBoard(i, j, index):
+            if index == len(word):
                 return True
-
+            
             for x, y in DIRS:
-                new_i, new_j = x + i, y + j
-                if inBounds(new_i, new_j) and board[new_i][new_j] == word[count]:
-                    board[new_i][new_j] = -1
-                    if backtrack(new_i, new_j, count + 1):
-                        return True
-                    board[new_i][new_j] = word[count]
-
+                new_i, new_j = i + x, j + y
+                if inBounds(new_i, new_j) and word[index] == board[new_i][new_j]:
+                    curr_char = board[new_i][new_j]
+                    board[new_i][new_j] = 0
+                    if traverseBoard(new_i, new_j, index + 1):  return True
+                    board[new_i][new_j] = curr_char
+                    
             return False
         
         for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    board[i][j] = -1
-                    if backtrack(i, j, 1):
-                        return True
-                    board[i][j] = word[0]
+            for j in range(len(board[i])):
+                curr_char = board[i][j]
+                if curr_char == word[0]:
+                    board[i][j] = 0
+                    if traverseBoard(i, j, 1):  return True
+                    board[i][j] = curr_char
         
         return False
